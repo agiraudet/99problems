790b8517b3ee24b4816389aa318473974a78a658
agiraudet <alois.giraudet+github@gmail.com>
2024-09-25 18:04:48 +0200

    ongoing

--- lib/pbs.ml
+++ lib/pbs.ml
@@ -1,92 +1,99 @@
+(* HELPERS *)
 let print_opt = function
   | None -> print_endline "None"
   | Some x -> print_endline x
 
-let range lo hi =
-  let rec aux hi acc = if hi < lo then acc else aux (hi - 1) (hi :: acc) in
-  aux (hi - 1) []
+(* 99 Problems *)
 
-let rec tail_of_a_list = function
-  | [] -> None
-  | [x] -> Some x
-  | _ :: t -> tail_of_a_list t
+(* Tail of a List *)
+let rec last = function [] -> None | [ x ] -> Some x | _ :: t -> last t
 
-let rec two_elements_of_a_list = function
-  | [] | [_] -> None
-  | [a; b] -> Some (a, b)
-  | _ :: t -> two_elements_of_a_list t
+(* Last Two Elements of a List *)
+let rec last_two = function
+  | [] | [ _ ] -> None
+  | [ a; b ] -> Some (a, b)
+  | _ :: t -> last_two t
 
-let rec nth_element_of_a_list lst n =
+(* N'th Element of a List *)
+let rec nth lst n =
   match lst with
   | [] -> failwith "nth"
-  | h :: t -> if n = 0 then h else nth_element_of_a_list t (n - 1)
+  | h :: t -> if n = 0 then h else nth t (n - 1)
 
-let length_of_a_list lst =
+(*  Length of a List *)
+let length lst =
   let rec aux acc = function [] -> acc | _ :: t -> aux (acc + 1) t in
   aux 0 lst
 
-let reverse_a_list lst =
+(* Reverse a List *)
+let rev lst =
   let rec aux acc = function [] -> acc | h :: t -> aux (h :: acc) t in
   aux [] lst
 
-let palindrome lst = if reverse_a_list lst = lst then true else false
+(* Palindrome *)
+let is_palindrome lst = if rev lst = lst then true else false
 
+(*  Flatten a List *)
 type 'a node = One of 'a | Many of 'a node list
 
-let flatten_a_list lst =
+let flatten lst =
   let rec aux acc = function
     | [] -> acc
     | h :: t -> (
-      match h with
-      | One x -> aux (x :: acc) t
-      | Many x -> aux (aux [] x @ acc) t )
+        match h with
+        | One x -> aux (x :: acc) t
+        | Many x -> aux (aux [] x @ acc) t)
   in
-  reverse_a_list @@ aux [] lst
+  rev @@ aux [] lst
 
-let flatten_a_list2 lst =
+let flatten2 lst =
   let rec aux acc = function
     | [] -> acc
     | One x :: t -> aux (x :: acc) t
     | Many m :: t -> aux (aux acc m) t
   in
-  reverse_a_list @@ aux [] lst
+  rev @@ aux [] lst
 
-let eliminate_duplicate lst =
+(*  Eliminate Duplicates *)
+let compress lst =
   let is_in x = function [] -> false | h :: _ -> x = h in
   let rec aux acc = function
     | [] -> acc
     | h :: t -> aux (if is_in h acc then acc else h :: acc) t
   in
-  reverse_a_list @@ aux [] lst
+  rev @@ aux [] lst
 
-let pack_consecutive_duplicate lst =
-  let rec pack acc lst =
+(* Pack Consecutive Duplicates *)
+let pack lst =
+  let rec inpack acc lst =
     match lst with
     | [] -> (acc, lst)
     | h :: t as l -> (
-      match acc with
-      | [] -> pack (h :: acc) t
-      | u :: _ -> if h = u then pack (h :: acc) t else (acc, l) )
+        match acc with
+        | [] -> inpack (h :: acc) t
+        | u :: _ -> if h = u then inpack (h :: acc) t else (acc, l))
   in
   let rec aux acc = function
     | [] -> acc
     | l ->
-        let a, lt = pack [] l in
+        let a, lt = inpack [] l in
         aux (a :: acc) lt
   in
-  reverse_a_list @@ aux [] lst
+  rev @@ aux [] lst
 
-let run_length_encoding lst =
+(* Run-Length Encoding *)
+let encode lst =
   let rec aux acc n cur = function
     | [] -> (n, cur) :: acc
     | h :: t ->
         if h = cur then aux acc (n + 1) h t else aux ((n, cur) :: acc) 1 h t
   in
-  match lst with [] -> [] | h :: t -> reverse_a_list @@ aux [] 1 h t
+  match lst with [] -> [] | h :: t -> rev @@ aux [] 1 h t
 
+(* Modified Run-Length Encoding *)
 type 'a rle = One of 'a | Many of int * 'a
 
-let modified_run_length_encoding lst =
+let mod_encode lst =
   let make_rle n cur = if n > 1 then Many (n, cur) else One cur in
   let rec aux acc n cur = function
     | [] -> make_rle n cur :: acc
@@ -94,13 +101,77 @@ let modified_run_length_encoding lst =
         if h = cur then aux acc (n + 1) h t
         else aux (make_rle n cur :: acc) 1 h t
   in
-  match lst with [] -> [] | h :: t -> reverse_a_list @@ aux [] 1 h t
+  match lst with [] -> [] | h :: t -> rev @@ aux [] 1 h t
 
-let decode_a_run_lenght_encoded_list lst =
+(* Decode a Run-Length Encoded List *)
+let decode lst =
   let make_rle n cur = if n > 1 then Many (n, cur) else One cur in
   let rec aux acc = function
     | [] -> acc
     | One x :: t -> aux (x :: acc) t
     | Many (n, x) :: t -> aux (x :: acc) (make_rle (n - 1) x :: t)
   in
-  reverse_a_list @@ aux [] lst
+  rev @@ aux [] lst
+
+(* Duplicate the Elements of a List *)
+let duplicate lst =
+  let rec aux acc = function [] -> acc | h :: t -> aux (h :: h :: acc) t in
+  rev @@ aux [] lst
+
+(* Replicate the Elements of a List a Given Number of Times *)
+let replicate lst n =
+  let rec dup acc e = function 0 -> acc | x -> dup (e :: acc) e (x - 1) in
+  let rec aux acc = function [] -> acc | h :: t -> aux (dup [] h n @ acc) t in
+  rev @@ aux [] lst
+
+(* Drop Every N'th Element From a List *)
+let drop lst n =
+  let rec aux acc i = function
+    | [] -> acc
+    | h :: t -> if i = n then aux acc 1 t else aux (h :: acc) (i + 1) t
+  in
+  rev @@ aux [] 1 lst
+
+(* Split a List Into Two Parts; The Length of the First Part Is Given *)
+let split lst len_a =
+  let rec aux acc n = function
+    | [] -> (rev acc, [])
+    | h :: t -> if n < len_a then aux (h :: acc) (n + 1) t else (rev acc, h :: t)
+  in
+  aux [] 0 lst
+
+(* Extract a Slice From a List *)
+let slice lst i k =
+  let rec aux acc n = function
+    | [] -> acc
+    | h :: t ->
+        if n > k then acc
+        else if n >= i then aux (h :: acc) (n + 1) t
+        else aux acc (n + 1) t
+  in
+  rev @@ aux [] 0 lst
+
+(* Rotate a List N Places to the Left *)
+let rotate lst n =
+  let l, r = split lst n in
+  r @ l
+
+(* Remove the K'th Element From a List *)
+let remove_at k lst =
+  let rec aux acc n = function
+    | [] -> List.rev acc
+    | h :: t -> if n = k then List.rev acc @ t else aux (h :: acc) (n + 1) t
+  in
+  aux [] 0 lst
+
+(* Insert an Element at a Given Position Into a List *)
+let insert_at e p = function
+  | [] -> [ e ]
+  | lst ->
+      let l, r = split lst p in
+      l @ (e :: r)
+
+(* Create a List Containing All Integers Within a Given Range *)
+let range lo hi =
+  let rec aux hi acc = if hi < lo then acc else aux (hi - 1) (hi :: acc) in
+  aux hi []
